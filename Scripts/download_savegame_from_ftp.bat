3e68c39f7ac970eea83c23f743e6dc40c36b6ffa
FFrese90 <FabianFrese12@hotmail.com>
2024-12-23 13:00:01 +0100

    fix: fix download starter batch

--- Scripts/download_savegame_from_ftp.bat
+++ Scripts/download_savegame_from_ftp.bat
@@ -2,6 +2,6 @@ echo ------------------
 echo Run Python download_from_ftp_server.py
 echo ------------------
 pushd private
-python clear_and_upload_to_ftp_server.py
+python download_from_ftp_server.py
 popd
 pause
